f401604e54cc8540bd8567d87806384ab0610290
Thusitha Wijethunga <thusithawijethunga@gmail.com>
2024-04-08 21:53:49 +0530

    build(deps): create composer.json and notification-telegram.php

---
+++ config/notification-telegram.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'api_url' => env('TELEGRAM_API_URL', 'https://telegram.promoxp.us/api/create-message'),
+    'appkey' => env('TELEGRAM_APPKEY', ''),
+    'authkey' => env('TELEGRAM_AUTHKEY', ''),
+];
